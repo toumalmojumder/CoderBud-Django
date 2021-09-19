85d15a09c228b1f33ba65de93a691a85eb274084
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2021-09-19 10:42:22 +0600

    update delete
    80% so far

--- CoderBud_django/employee_entry/urls.py
+++ CoderBud_django/employee_entry/urls.py
@@ -3,6 +3,9 @@ from django.urls.conf import include
 from . import views
 
 urlpatterns = [
-    path('', views.tada_form),
-    path('list/', views.tada_list)
+    path('', views.tada_form, name ='employee_tada_add'),
+    path('<int:id>/', views.tada_form,name='tada_update'),
+    path('delete<int:id>/', views.tada_delete,name='tada_delete'),
+    path('list/', views.tada_list, name= 'employee_tada_list')
+
 ]
--- CoderBud_django/employee_entry/views.py
+++ CoderBud_django/employee_entry/views.py
@@ -7,12 +7,25 @@ def tada_list(request):
     context ={'tada_list':TADA.objects.all()}
     return render(request,"tada_entry/tada_list.html",context) 
 
-def tada_form(request):
+def tada_form(request, id = 0):
     if request.method == "GET":
-        form = EmployeeTaDaForm()
+        if id== 0:
+            form = EmployeeTaDaForm()
+        else:
+            tada = TADA.objects.get(pk=id)
+            form = EmployeeTaDaForm(instance = tada)
         return render(request,"tada_entry/tada_form.html", {'form':form})
     else:
-        form = EmployeeTaDaForm(request.POST)
-        if form.is_valid():
+        if id == 0:
+            form = EmployeeTaDaForm(request.POST)
+        else:
+            tada = TADA.objects.get(pk=id)
+            form = EmployeeTaDaForm(request.POST, instance = tada)
+        if  form.is_valid():
             form.save()
         return redirect('/tada/list')
+
+def tada_delete(request,id):
+    tada = TADA.objects.get(pk=id)
+    tada.delete()
+    return redirect('/tada/list')
