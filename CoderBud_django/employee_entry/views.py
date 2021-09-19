56daea5eeb09d8114061ee453d96d964b6f38b05
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2021-09-20 02:25:00 +0600

    last commit

--- CoderBud_django/employee_entry/views.py
+++ CoderBud_django/employee_entry/views.py
@@ -6,7 +6,7 @@ import io
 from reportlab.pdfgen import canvas, textobject
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter 
-
+from django.db.models import Sum
 
 
 # Create your views here.
@@ -43,18 +43,40 @@ def generate_pdf(request):
     c = canvas.Canvas(buf,pagesize=letter, bottomup=0)
     textob = c.beginText()
     textob.setTextOrigin(inch,inch)
-    textob.setFont("Helvetica", 14)
+    textob.setFont("Helvetica", 10)
 
 
 
     tadas =TADA.objects.all()
 
-    lines = []
-    lines.append("Date          Name            Travel Cost         Lunch Cost          Instrument Cost         Paid")
-    for tada in tadas:
-        lines.append(str(tada.date) + "            " + str(tada.employee_name) + "           " + str(tada.travel_cost) + "         " + str(tada.lunch_cost) + "          " + str(tada.instruments_cost) + "            " + str(tada.paid))
-        lines.append("________________________________________________________________________________________________")
+  
+    travel_cost_sum = TADA.objects.aggregate(Sum('travel_cost'))
+    total_travel_cost = travel_cost_sum['travel_cost__sum']
 
+    lunch_cost_sum = TADA.objects.aggregate(Sum('lunch_cost'))
+    total_lunch_cost = lunch_cost_sum['lunch_cost__sum']
+
+    instruments_cost_sum = TADA.objects.aggregate(Sum('instruments_cost'))
+    total_instruments_cost = instruments_cost_sum['instruments_cost__sum']
+
+    #net_cost = float(str(total_travel_cost))+float(str(total_lunch_cost))+float(str(total_instruments_cost))
+    net_cost = total_travel_cost+total_lunch_cost+total_instruments_cost
+    lines = []
+    
+    lines.append("Date                      Name            Travel Cost         Lunch Cost          Instrument Cost         Paid")
+    for tada in tadas:
+        lines.append(str(tada.date) + "            " + str(tada.employee_name) + "                " + str(tada.travel_cost) + "               " + str(tada.lunch_cost) + "                       " + str(tada.instruments_cost) + "                   " + str(tada.paid))
+        lines.append("_______________________________________________________________________")
+
+    lines.append("")
+    lines.append("Total travel cost:" + str(total_travel_cost))
+    lines.append("")
+    lines.append("Total lunch cost:" + str(total_lunch_cost))
+    lines.append("")
+    lines.append("Total instruments cost:" + str(total_instruments_cost))
+    lines.append("")
+    lines.append("Net cost:" + str(net_cost))
+   
     for line in lines:
         textob.textLine(line)
     c.drawText(textob)
